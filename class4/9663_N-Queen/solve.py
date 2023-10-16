32d0efa8f80f66055433a33a65d4cc83138a7678
박성민 <sm136599@gmail.com>
2023-10-16 23:48:47 +0900

    fix 9663 but still with pypy3

--- class4/9663_N-Queen/solve.py
+++ class4/9663_N-Queen/solve.py
@@ -1,50 +1,32 @@
 import sys
-import time
-
 
 readl = sys.stdin.readline
 
 N = int(readl())
-t_s = time.time()
 cnt = 0
-stack = []
+board = [0] * N
 visited = [False] * N
 
-def isOk(stack, N, l, m):
-    for mm in range(m+1, N*2):
-        ll = l + m - mm
-        if 0 <= ll < N:
-            if stack[ll] == mm or stack[ll] == m*2 - mm:
-                return False
+def isOk(x):
+    for i in range(x):
+        if board[i] == board[x] or abs(board[i] - board[x]) == abs(i - x):
+            return False
     return True
 
-def bt(i, j):
-    global cnt, stack, board, N, visited
+def bt(i):
+    global cnt, visited
 
-    if i == N-1:
+    if i == N:
         cnt += 1
         return 
     
     for k in range(N):
         if not visited[k]:
-            if stack[i] == k -1 or stack[i] == k+1:
-                continue
-            
-            if isOk(stack, N, i+1, k):
-                stack.append(k)
+            board[i] = k
+            if isOk(i):
                 visited[k] = True
-                bt(i+1, k)
-                stack.pop()
+                bt(i+1)
                 visited[k] = False
 
-    
-
-for i in range(N):
-    stack.append(i)
-    visited[i] = True
-    bt(0, i)
-    stack.pop()
-    visited[i] = False
-
+bt(0)
 print(cnt)
-print(time.time() - t_s)
