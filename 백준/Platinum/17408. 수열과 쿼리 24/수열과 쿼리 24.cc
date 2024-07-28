fa6ca062156059aab50a8c59accff470cc147ce5
박성민 <74234978+sm136599@users.noreply.github.com>
2024-07-28 16:23:11 +0900

    [Platinum IV] Title: 수열과 쿼리 24, Time: 84 ms, Memory: 5548 KB -BaekjoonHub

--- 백준/Platinum/17408. 수열과 쿼리 24/수열과 쿼리 24.cc
+++ 백준/Platinum/17408. 수열과 쿼리 24/수열과 쿼리 24.cc
@@ -3,33 +3,31 @@ using namespace std;
 
 class SegmentTree {
 	vector<pair<int, int>> tree;
-	vector<int> original;
 	int size;
 public:
 	SegmentTree(vector<int>& input) {
-		original = input;
 		size = input.size();
 		tree = vector<pair<int, int>>(input.size() * 4);
-		generate(1, 0, size - 1);
+		generate(1, 0, size - 1, input);
 	}
-	pair<int, int> generate(int idx, int start, int end) {
-		if (start == end) return tree[idx] = make_pair(original[start], 0);
+	pair<int, int> generate(int idx, int start, int end, vector<int>& input) {
+		if (start == end) return tree[idx] = make_pair(input[start], 0);
 		int mid = (start + end) / 2;
-		return tree[idx] = getFS(generate(idx * 2, start, mid), generate(idx * 2 + 1, mid + 1, end));
+		return tree[idx] = getFS(generate(idx * 2, start, mid, input),  
+			                     generate(idx * 2 + 1, mid + 1, end, input));
 	}
 
 	void update(int index, int num) {
-		original[index] = num;
 		updateTree(1, 0, size - 1, index, num);
 	}
 
 	pair<int, int> updateTree(int idx, int start, int end, int target, int changeN) {
 		if (target < start || end < target) return tree[idx];
-		if (start == end && start == target) return tree[idx] = make_pair(original[target], 0);
+		if (start == end && start == target) return tree[idx] = make_pair(changeN, 0);
 		if (start == end) return tree[idx];
 		int mid = (start + end) / 2;
 		return tree[idx] = getFS(updateTree(idx * 2, start, mid, target, changeN),
-							updateTree(idx * 2 + 1, mid + 1, end, target, changeN));
+							     updateTree(idx * 2 + 1, mid + 1, end, target, changeN));
 	}
 
 	int query(int left, int right) {
@@ -41,28 +39,20 @@ public:
 		if (left > end || right < start) return make_pair(0, 0);
 		if (left <= start && right >= end) return tree[idx];
 		int mid = (start + end) / 2;
-		pair<int, int> a = queryTree(idx * 2, start, mid, left, right);
-		pair<int, int> b = queryTree(idx * 2 + 1, mid + 1, end, left, right);
-		if (a.first == -1) return b;
-		else if (b.first == -1) return a;
-		else return getFS(a, b);
+		return getFS(queryTree(idx * 2, start, mid, left, right), 
+			         queryTree(idx * 2 + 1, mid + 1, end, left, right));
 	}
 
 	pair<int, int> getFS(const pair<int, int>& a, const pair<int, int>& b) {
-		vector<int> temp;
-		temp.emplace_back(a.first);
-		temp.emplace_back(a.second);
-		temp.emplace_back(b.first);
-		temp.emplace_back(b.second);
-		sort(temp.begin(), temp.end(), greater<>());
-		return make_pair(temp[0], temp[1]);
+		if (a.first >= b.first) {
+			return make_pair(a.first, max(b.first, a.second));
+		}
+		else {
+			return make_pair(b.first, max(a.first, b.second));
+		}
 	}
 
 	void print() {
-		for (int i = 0; i < original.size(); i++) {
-			cout << original[i] << " ";
-		}
-		cout << '\n';
 		int h = 1;
 		while (pow(2, h) <= tree.size()) {
 			for (int j = pow(2, h - 1); j < pow(2, h); j++) {
