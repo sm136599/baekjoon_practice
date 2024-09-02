a1c209a39cd2d499bb6dc02158f26b8a286c1cec
박성민 <74234978+sm136599@users.noreply.github.com>
2024-09-02 18:04:47 +0900

    Update 기지국 설치.cpp

--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.cpp
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.cpp
@@ -16,16 +16,12 @@ int getCount(int start, int end, int w) {
 int solution(int n, vector<int> stations, int w)
 {
     int answer = 0;
-    int len = stations.size();
-    int start = 0;
-    int end = stations[0] - 1 - w - 1;
-    answer += getCount(start, end, w);
-    start = stations[0] - 1 + w + 1;
-    for (int i = 1; i < len; i++) {
-        end = stations[i] - 1 - w - 1;
+    int start = 1, end;
+    for (int i = 0; i < stations.size(); i++) {
+        end = stations[i] - w - 1;
         answer += getCount(start, end, w);
-        start = stations[i] - 1 + w + 1;
+        start = stations[i] + w + 1;
     }
-    answer += getCount(start, n-1, w);
+    answer += getCount(start, n, w);
     return answer;
 }
