03c3f53203ab44f49aaf8ade0f3828988feb5574
박성민 <74234978+sm136599@users.noreply.github.com>
2024-09-05 23:14:10 +0900

    [level 3] Title: 인사고과, Time: 177.50 ms, Memory: 21.4 MB -BaekjoonHub

--- 프로그래머스/3/152995. 인사고과/인사고과.cpp
+++ 프로그래머스/3/152995. 인사고과/인사고과.cpp
@@ -10,6 +10,7 @@ int solution(vector<vector<int>> scores) {
     int answer = 1;
     int getIncentive = true;
     int n = scores.size();
+    priority_queue<int> pq;
     int target = scores[0][0] + scores[0][1];
     for (int i = 1; i < n; i++) {
         if (scores[0][0] < scores[i][0] && scores[0][1] < scores[i][1]) {
@@ -17,8 +18,7 @@ int solution(vector<vector<int>> scores) {
         }
     }
     sort(scores.begin(), scores.end());
-    for (int i = 0; i < n; i++) {
-        if (target >= scores[i][0] + scores[i][1]) continue;
+    for (int i = 0; i < n - 1; i++) {
         bool flag = true;
         for (int j = n-1; j > i; j--) {
             if (scores[i][0] < scores[j][0] && scores[i][1] < scores[j][1]) {
@@ -27,11 +27,15 @@ int solution(vector<vector<int>> scores) {
             }
             if (scores[i][0] == scores[j][0]) break;
         }
-        if (flag) {
-            if (target < scores[i][0] + scores[i][1]) {
-                answer++;
-            }
-        }
+        if (flag)
+            pq.push(scores[i][0] + scores[i][1]);
+    }
+    pq.push(scores[n-1][0] + scores[n-1][1]);
+    
+    while (!pq.empty()) {
+        if (target == pq.top()) break;
+        pq.pop();
+        answer++;
     }
     return answer;
 }
