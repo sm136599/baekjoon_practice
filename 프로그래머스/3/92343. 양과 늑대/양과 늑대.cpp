18e58218df8cfa48f4b522362ecda507f7b12b6d
박성민 <74234978+sm136599@users.noreply.github.com>
2024-11-12 00:10:25 +0900

    [level 3] Title: 양과 늑대, Time: 22.07 ms, Memory: 30 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.cpp
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.cpp
@@ -16,22 +16,22 @@ int solution(vector<int> info, vector<vector<int>> edges) {
         graph[edge[1]].push_back(edge[0]);
     }
     int iter = 0;
+    vector<vector<vector<vector<bool>>>> visited_state(sheep_count+1, vector<vector<vector<bool>>>(info.size()-sheep_count+1, vector<vector<bool>>(info.size(), vector<bool>(1<<(info.size())))));
     // 양 수, 늑대 수, 방문여부, 현재 노드
-    // priority_queue<tuple<int, int, int, int>> pq;
-    queue<tuple<int, int, int, int>> pq;
-    pq.emplace(1, 0, 1, 0);
-    while (!pq.empty()) {
-        if (iter++ >= 25000000) break;
+    queue<tuple<int, int, int, int>> q;
+    q.emplace(1, 0, 1, 0);
+    while (!q.empty()) {
         if (sheep_count == answer) break;
-        auto [sheep, wolf, visited, cur] = pq.front();
-        pq.pop();
-        // cout << sheep << ' ' << wolf << ' ' << cur << ' ' << visited << '\n';
+        auto [sheep, wolf, visited, cur] = q.front();
+        q.pop();
         answer = max(answer, sheep);
         for (auto next : graph[cur]) {
             int next_sheep = sheep + ((visited & (1 << next))?0:!info[next]);
             int next_wolf = wolf + ((visited & (1 << next))?0:info[next]);
             if (next_sheep <= next_wolf) continue;
-            pq.emplace(next_sheep, next_wolf, visited | (1 << next), next);
+            if (visited_state[next_sheep][next_wolf][next][visited | (1 << next)]) continue;
+            visited_state[sheep][wolf][next][visited] = true;
+            q.emplace(next_sheep, next_wolf, visited | (1 << next), next);
         }
     }
     
