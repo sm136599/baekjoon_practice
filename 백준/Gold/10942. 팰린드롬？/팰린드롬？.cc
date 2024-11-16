3b51a6de6a29febc993e0721047c1235c5da7d69
박성민 <74234978+sm136599@users.noreply.github.com>
2024-11-17 00:09:34 +0900

    [Gold IV] Title: 팰린드롬?, Time: 200 ms, Memory: 2760 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.cc
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.cc
@@ -8,9 +8,8 @@ int main(void) {
 	vector<int> nums(N);
 	for (int i = 0; i < N; i++) cin >> nums[i];
 	vector<vector<bool>> dp(N, vector<bool>(N, true));
-	for (int k = 1; k < N; k++) {
-		for (int i = 0; i + k < N; i++) {
-			int j = i + k;
+	for (int i = N - 2; i >= 0; i--) {
+		for (int j = i + 1; j < N; j++) {
 			dp[i][j] = (dp[i + 1][j - 1] && !(nums[i] ^ nums[j]));
 		}
 	}
