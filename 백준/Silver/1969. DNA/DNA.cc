4fb56bd5fec3763e9b84996bd1164f5a34584977
박성민 <74234978+sm136599@users.noreply.github.com>
2024-11-17 14:27:55 +0900

    [Silver IV] Title: DNA, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1969. DNA/DNA.cc
@@ -0,0 +1,48 @@
+#include "bits/stdc++.h"
+using namespace std;
+#define sws cin.tie(NULL)->ios::sync_with_stdio(false)
+
+#define A 0
+#define C 1
+#define G 2
+#define T 3
+
+int main(void) {
+	sws;
+	int N, M; cin >> N >> M;
+	vector<vector<unsigned short>> dnas(4, vector<unsigned short>(M));
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			char c; cin >> c;
+			switch (c)
+			{
+			case 'A':
+				dnas[A][j]++; break;
+			case 'T':
+				dnas[T][j]++; break;
+			case 'G':
+				dnas[G][j]++; break;
+			case 'C':
+				dnas[C][j]++; break;
+			default:
+				break;
+			}
+		}
+	}
+	int hamming_distance = 0;
+	string result;
+	for (int j = 0; j < M; j++) {
+		char max_char;
+		unsigned short count = 0;
+		for (int i = 0; i < 4; i++) {
+			if (count < dnas[i][j]) {
+				count = dnas[i][j];
+				max_char = (i == 0) ? 'A' : (i == 1) ? 'C' : (i == 2) ? 'G' : (i == 3) ? 'T' : ' ';
+			}
+		}
+		result += max_char;
+		hamming_distance += N - count;
+	}
+	cout << result << '\n' << hamming_distance;
+	return 0;
+}
