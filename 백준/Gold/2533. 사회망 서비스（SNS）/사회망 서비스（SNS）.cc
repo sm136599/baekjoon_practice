4771b233266fa4d24ffbb44812f0a07c7516fb4f
박성민 <74234978+sm136599@users.noreply.github.com>
2024-12-22 14:28:59 +0900

    [Gold III] Title: 사회망 서비스(SNS), Time: 588 ms, Memory: 57504 KB -BaekjoonHub

--- 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.cc
+++ 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.cc
@@ -3,29 +3,27 @@ using namespace std;
 #define sws cin.tie(NULL)->ios::sync_with_stdio(false)
 
 int N, answer = 0;
-vector<bool> visited;
-vector<bool> isEarlyAdopter;
+vector<char> visited;
 vector<vector<int>> network;
 
-bool findEarlyAdopter(int node) {
-	visited[node] = true;
+int findEarlyAdopter(int node) {
+	visited[node] = 1;
 
-	bool isAllFriendsEarlyAdopter = true;
+	int isAllFriendsEarlyAdopter = 1;
 	for (auto& next : network[node]) {
 		if (visited[next]) continue;
 		isAllFriendsEarlyAdopter &= findEarlyAdopter(next);
 	}
-	if (isAllFriendsEarlyAdopter) isEarlyAdopter[node] = false;
-	answer += isEarlyAdopter[node];
-	return isEarlyAdopter[node];
+	answer += isAllFriendsEarlyAdopter ^ 1;
+	return isAllFriendsEarlyAdopter ^ 1;
 }
 
 int main(void) {
 	sws;
 	cin >> N;
-	visited.resize(N);
-	isEarlyAdopter.resize(N, true);
-	network.resize(N);
+	visited.reserve(N);
+	network.reserve(N);
+	for (auto& v : network) v.reserve(N);
 	for (int i = 1; i < N; i++) {
 		int u, v; cin >> u >> v;
 		network[u - 1].push_back(v - 1);
