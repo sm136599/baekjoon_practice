aa8f5996c312675a215972369054deb450c1d36b
박성민 <74234978+sm136599@users.noreply.github.com>
2025-01-08 20:44:27 +0900

    [Gold V] Title: 토마토, Time: 76 ms, Memory: 10580 KB -BaekjoonHub

---
+++ 백준/Gold/7576. 토마토/토마토.cc
@@ -0,0 +1,67 @@
+#include "bits/stdc++.h"
+using namespace std;
+#define sws cin.tie(NULL)->ios::sync_with_stdio(false)
+
+/*
+* 시간이 지날 수록 주변에 토마토에 영향을 끼치는 문제
+* 시간이지나 스스로 익는 토마토는 없기 때문에, 
+* 단순히 현재 시간과 익은 토마토의 위치를 넣고 빼는 큐를 사용해 BFS를 사용해 풀면된다.
+* 시간 복잡도는 O(M*N)이다.
+*/
+
+int di[]{ 0, 0, 1, -1 };
+int dj[]{ 1, -1, 0, 0 };
+
+int main(void) {
+	sws;
+	int M, N; cin >> M >> N;
+	vector<vector<char>> tomatos(N, vector<char>(M));
+	vector<vector<char>> visited(N, vector<char>(M));
+	queue<tuple<int, int, int>> q;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			int n; cin >> n;
+			tomatos[i][j] = n;
+			if (n == 1) {
+				q.emplace(0, i, j);
+				visited[i][j] = 1;
+			}
+		}
+	}
+
+	int answer = 0;
+	while (!q.empty()) {
+		auto [time, ci, cj] = q.front(); q.pop();
+		answer = max(answer, time);
+
+		for (int k = 0; k < 4; k++) {
+			int ni = ci + di[k];
+			int nj = cj + dj[k];
+			if (!(0 <= ni && ni < N && 0 <= nj && nj < M)) continue;
+			if (visited[ni][nj]) continue;
+			if (tomatos[ni][nj] == -1) continue;
+			visited[ni][nj] = 1;
+			tomatos[ni][nj] = 1;
+			q.emplace(time + 1, ni, nj);
+		}
+	}
+
+	int tomato = 0;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			if (tomatos[i][j] == 0) {
+				tomato = 1;
+				break;
+			}
+		}
+	}
+	if (tomato) {
+		cout << -1;
+	}
+	else {
+		cout << answer;
+	}
+
+
+	return 0;
+}
