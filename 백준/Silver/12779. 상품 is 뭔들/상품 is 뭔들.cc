54da266b64b0c1036cf437eb8389d930efe80703
박성민 <74234978+sm136599@users.noreply.github.com>
2025-01-14 22:43:48 +0900

    [Silver III] Title: 상품 is 뭔들, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/12779. 상품 is 뭔들/상품 is 뭔들.cc
+++ 백준/Silver/12779. 상품 is 뭔들/상품 is 뭔들.cc
@@ -4,13 +4,28 @@ using namespace std;
 
 typedef long long ll;
 
+ll llsqrt(ll val) {
+	ll s = 0, e = 1073741824;
+	ll ret;
+	while (s <= e) {
+		ll m = (s + e) / 2;
+		if (m * m <= val) {
+			s = m + 1;
+			ret = m;
+		}
+		else {
+			e = m - 1;
+		}
+	}
+	return ret;
+}
+
 int main(void) {
 	sws;
 	ll a, b; cin >> a >> b;
-	ll count = 0;
-	for (ll i = 1; i * i <= b; i++) {
-		if (i * i > a) count++;
-	}
+	ll s = 0, e = b;
+	ll a_sqrt = llsqrt(a), b_sqrt = llsqrt(b);
+	ll count = b_sqrt - a_sqrt;
 	ll g = gcd(count, b - a);
 	if (count) cout << count / g << '/' << (b - a) / g;
 	else cout << 0;
